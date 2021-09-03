3835f02238949de3854fcd63ce3c71e0d4b3f818
Don Spire <don.spire1@gmail.com>
2021-09-03 11:16:29 -0700

    add Login & Register to Navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -11,6 +11,8 @@ const Navbar = () => {
         <li>
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
+          <Link to="/register">Register</Link>
+          <Link to="/login">Login</Link>
         </li>
       </ul>
     </div>
